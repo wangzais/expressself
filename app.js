e0bd9df8e629bac77a6a349b18db3e6ad770b4eb
wangyang <18503956773@163.com>
2020-06-08 19:52:52 +0800

    'webdist'

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var proxy = require('express-http-proxy');
-const HOST = 'http://10.0.0.132:9810'
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var proxy = require("express-http-proxy");
+var router = express.Router();
+const HOST = "http://10.0.0.132:9810";
 var app = express();
 var options = {
-  target: 'http://10.0.0.132:9810',
+  target: "http://10.0.0.132:9810",
   changeOrigin: true,
   pathRewrite: {
-    '^/api': 'http://10.0.0.132:9810/',
+    "^/api": "http://10.0.0.132:9810",
   },
   preserveHostHdr: true,
   reqAsBuffer: true,
-  // //转发之前触发该方法
+  //转发之前触发该方法
   // proxyReqPathResolver: function (req, res) {
-  //   return `/api${req.url}`;
+  //   console.log(req);
+  //   return `${req.url}`;
   // },
-
 };
+router.post("/", function (req, res, next) {
+  var path = req.originalUrl;
+  var content = req.body;
+  console.log(path, content);
+});
 var optionss = {
-  target: 'http://10.0.0.132:9810',
+  target: "http://10.0.0.132:9810",
   changeOrigin: true,
   pathRewrite: {
-    '^/': 'http://10.0.0.132:9810/',
+    "^/": "http://10.0.0.132:9810",
   },
   preserveHostHdr: true,
   reqAsBuffer: true,
@@ -31,26 +37,25 @@ var optionss = {
   // proxyReqPathResolver: function (req, res) {
   //   return `/api${req.url}`;
   // },
-
 };
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 // app.use('/', proxy(HOST, options));
-app.use('/api', proxy(HOST, options));
-app.use('/', proxy(HOST, optionss));
+app.use("/api", proxy(HOST, options));
+app.use("/", proxy(HOST, optionss));
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
---
+++ public/precache-manifest.7e66555497dcbe3cb70e63bf3d394345.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dff2c16f9b2a4ee2e91173a6206f2fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e50b82948752a3f2af8d",
+    "url": "./static/css/2.da6710a0.chunk.css"
+  },
+  {
+    "revision": "72bab9ebc126c4828c5f",
+    "url": "./static/css/main.23ac4024.chunk.css"
+  },
+  {
+    "revision": "e50b82948752a3f2af8d",
+    "url": "./static/js/2.5d7993fc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.5d7993fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72bab9ebc126c4828c5f",
+    "url": "./static/js/main.bab83420.chunk.js"
+  },
+  {
+    "revision": "874cdbc76e16ebe9448c",
+    "url": "./static/js/runtime-main.7d1e0f1a.js"
+  },
+  {
+    "revision": "4f32aafd1ca4d1f169d0060b6d384b84",
+    "url": "./static/media/bg_1.4f32aafd.png"
+  },
+  {
+    "revision": "5d681bde701382b3d73707421cafcb06",
+    "url": "./static/media/bg_2.5d681bde.png"
+  },
+  {
+    "revision": "0a69f61ca3bf45f999a1aa9b38ed5871",
+    "url": "./static/media/iconfont.0a69f61c.ttf"
+  },
+  {
+    "revision": "235756f8a5bab92cbad7681d23b7dc4f",
+    "url": "./static/media/iconfont.235756f8.eot"
+  },
+  {
+    "revision": "53e23dbbaf5ff3b59ab216da5366a83c",
+    "url": "./static/media/iconfont.53e23dbb.svg"
+  },
+  {
+    "revision": "8b0e171f7feff31d8ed5b888842eabe2",
+    "url": "./static/media/iconfont.8b0e171f.svg"
+  },
+  {
+    "revision": "8d70bd5ea06c938ecd3dd5d364012b57",
+    "url": "./static/media/iconfont.8d70bd5e.ttf"
+  },
+  {
+    "revision": "9bd56e9a4ae5adde50849710e2ec7dd0",
+    "url": "./static/media/iconfont.9bd56e9a.woff"
+  },
+  {
+    "revision": "be28c5396d02f3d481181c435a4c3396",
+    "url": "./static/media/iconfont.be28c539.woff"
+  },
+  {
+    "revision": "f4c03dddcfb6744eacb6c42737bdde5d",
+    "url": "./static/media/iconfont.f4c03ddd.eot"
+  }
+]);
